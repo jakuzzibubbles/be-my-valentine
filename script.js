00ccc1113e1026510234c08debe3e84a3f3103cc
jakuzzibubbles <jakuzzi263@gmail.com>
2025-01-31 23:58:48 +0100

    more messages

--- script.js
+++ script.js
@@ -7,29 +7,29 @@ const gifImage = document.querySelector(".kitty");
 
 let clickCount = 0;
 const noTexts = [
-  "What?",
+  "Come on, don't be shy!",
   "Really?",
-  "Why not?",
-  "Think again!",
-  "You'll regret it!",
   "You are breaking my heart!",
   "Just say YES!",
-  "Last chance!",
-  "Come on, don't be shy!",
-  "Seriously?",
-  "I won't stop asking!",
+  "Why not?",
   "Please, just say yes! 😭",
+  "What?",
+  "You'll regret it!",
+  "Think again!",
+  "Seriously?",
+  "Last chance!",
+  "No",
 ];
 
 noBtn.addEventListener("click", function () {
   if (clickCount < noTexts.length) {
-    yesBtn.style.transform = `scale(${1 + clickCount * 0.2})`;
+    yesBtn.style.transform = `scale(${1 + clickCount * 0.1})`;
     noBtn.innerText = noTexts[clickCount];
-    noBtn.style.transform = `scale(${1 - clickCount * 0.1})`;
+    noBtn.style.transform = `scale(${1 - clickCount * 0.05})`;
     buttonsContainer.style.marginTop = `${10 + clickCount * 10}px`;
     clickCount++;
   } else {
-    yesBtn.style.transform = "scale(3)";
+    yesBtn.style.transform = "scale(5)";
     question.innerText = "OK, YAY!";
     noBtn.style.display = "none";
   }
@@ -38,6 +38,9 @@ noBtn.addEventListener("click", function () {
 yesBtn.addEventListener("click", function () {
     gifImage.src =
       "https://media.giphy.com/media/qFmdpUKAFZ6rMobzzu/giphy.gif?cid=ecf05e47mg5e73jkxu6ob2painy03ul3klhpub469af3gv41&ep=v1_gifs_search&rid=giphy.gif&ct=g";
-      question.innerText = "OK, YAY!";
-  message.innerText = "Yay! 🥰 Can't wait!";
+      question.innerText = "OK, Yay! 🥰 Can't wait!";
+
+      noBtn.style.display ="none";
+      yesBtn.style.margin = "0 auto";
+      yesBtn.style.display = "block";
 });
